4242d2634f65def70c85d39147eb455dd0edb1cf
Renan Colman <renan.colman@hotmail.com>
2017-10-04 16:07:25 -0300

    Atividade Pronta.

---
+++ app/src/main/java/com/example/arqdsis/atividade/DialogActivity.java
@@ -0,0 +1,27 @@
+package com.example.arqdsis.atividade;
+
+/**
+ * Created by arqdsis on 03/10/2017.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+    public class DialogActivity extends Activity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.activity_dialog);
+        }
+
+        /**
+         * Callback method defined by the View
+         * @param v
+         */
+        public void finishDialog(View v) {
+            DialogActivity.this.finish();
+        }
+    }
